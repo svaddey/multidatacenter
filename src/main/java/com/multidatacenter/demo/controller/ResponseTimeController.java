945b8b09fcce163156b2ad8e8518a8af8e4e6d65
svaddey <swamy.vaddey@gmail.com>
2020-06-23 01:07:46 +0530

    Add files via upload

--- src/main/java/com/multidatacenter/demo/controller/ResponseTimeController.java
+++ src/main/java/com/multidatacenter/demo/controller/ResponseTimeController.java
@@ -1,12 +1,11 @@
 package com.multidatacenter.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
+import com.multidatacenter.demo.exception.ResponseTimeException;
 import com.multidatacenter.demo.model.ResponseTime;
 import com.multidatacenter.demo.service.ResponseTimeService;
 
@@ -14,14 +13,14 @@ import com.multidatacenter.demo.service.ResponseTimeService;
 public class ResponseTimeController {
 	@Autowired
 	ResponseTimeService responseTimeService;
-	
+
 	@GetMapping({ "/", "/index" })
-    public ResponseEntity<ResponseTime> index() {
-        ResponseTime responseTime = responseTimeService.getResponseTime();
-        if (responseTime.getActual() > responseTime.getValue()) {
-        	throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Response time too long");
-        } else {
-        	return ResponseEntity.ok(responseTime);
-        }
-    }
+	public ResponseEntity<ResponseTime> index() {
+		ResponseTime responseTime = responseTimeService.getResponseTime();
+		if (responseTime.getActual() > responseTime.getValue()) {
+			throw new ResponseTimeException("Response time too long");
+		} else {
+			return ResponseEntity.ok(responseTime);
+		}
+	}
 }
